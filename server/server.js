6de647ee9b56338ed9913ba8db4dfcc757236d40
victorsteven <chikodi543@gmail.com>
2018-10-03 23:54:45 +0100

    Added POST /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User');
+
+
+var app = express();
+
+//with the code below, we can send json data to our express app
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    // console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc); //send this to the user
+    }, err => res.status(400).send(err));
+});
+
+app.listen(3000, () => {
+    console.log('Started at port 3000')
+});
+
+
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc)
+// }, (e) => {
+//     console.log('Unable to save todo', e);  
+// });
+
+
+// var user = new User({
+//     email: '   obi@gmail.com   '
+// });
+// user.save().then((result) => {
+//     console.log(result)
+// }, err => console.log(err))
